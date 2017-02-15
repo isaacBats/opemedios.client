d3a3ea78737ccb25c51beeeff00f5980edeb6b8e
isaacBats <disa.indus@gmail.com>
2017-02-15 01:03:19 -0600

    comentario

--- intranet/crear_reporte_xls_intranet.php
+++ intranet/crear_reporte_xls_intranet.php
@@ -867,7 +867,7 @@ $htmlcode = '<p><span style="font-size:medium; text-decoration:underline;">2.2 D
 
             $noticias_redes = $pdo->query("SELECT n.id_noticia FROM noticia n INNER JOIN noticia_int ni ON n.id_noticia=ni.id_noticia {$jquery_where} AND ni.is_social = 1")->fetch(\PDO::FETCH_ASSOC);
 
-            print_r(compact('query_entero', 'noticias_redes'));exit;
+            echo 'Estas son las notas <pre>'; print_r(compact('query_entero', 'noticias_redes'));exit;
 			// que hay en el SQL
 			//$htmlcode.='<tr><td>'.$query_entero.'</td></tr>';
             $base->execute_query($query_entero);

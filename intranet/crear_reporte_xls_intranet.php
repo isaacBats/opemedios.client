1099474a98023671569e47e9ca4efdf6cf6f9610
isaacBats <disa.indus@gmail.com>
2017-02-15 01:01:08 -0600

    Se obtienen las notas que son de redes

--- intranet/crear_reporte_xls_intranet.php
+++ intranet/crear_reporte_xls_intranet.php
@@ -863,7 +863,11 @@ $htmlcode = '<p><span style="font-size:medium; text-decoration:underline;">2.2 D
             $query[] .= "ORDER BY noticia.fecha DESC, noticia.id_noticia DESC";
             $query_entero = join(" ",$query);
 
-            print_r($query_entero);exit;
+            $pdo = getPDO();
+
+            $noticias_redes = $pdo->query("SELECT n.id_noticia FROM noticia n INNER JOIN noticia_int ni ON n.id_noticia=ni.id_noticia {$jquery_where} AND ni.is_social = 1")->fetch(\PDO::FETCH_ASSOC);
+
+            print_r(compact('query_entero', 'noticias_redes'));exit;
 			// que hay en el SQL
 			//$htmlcode.='<tr><td>'.$query_entero.'</td></tr>';
             $base->execute_query($query_entero);

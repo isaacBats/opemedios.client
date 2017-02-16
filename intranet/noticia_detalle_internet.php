c06c59787ba176dc6f60445ba687a4e7a4cfa0bb
isaacBats <disa.indus@gmail.com>
2017-02-15 23:50:09 -0600

    quitamos el debbuger

--- intranet/noticia_detalle_internet.php
+++ intranet/noticia_detalle_internet.php
@@ -87,7 +87,6 @@ $base->init();
                          WHERE
                          noticia.id_noticia = ".$_GET['id_noticia'];
 
-                         print_r($query); exit;
                         $base->execute_query($query);
                         $noticia = new SuperNoticia($base->get_row_assoc());
 
